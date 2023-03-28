8692d1e103b14e839eb78ad88a28494adfd4d526
vlad <nekita.avdoshin@gmail.com>
2023-03-28 10:20:42 +0300

    fix conflict

--- devicesList.js
+++ devicesList.js
@@ -1,3 +1,14 @@
+window.addEventListener('load', () => {
+  const selectPhoneContainer = document.getElementById('selectPhoneModelBlock');
+  const selectNotebookContainer = document.getElementById('selectNotebookModel');
+
+  selectPhoneContainer.classList.add('noDisplay');
+  selectPhoneContainer.classList.remove('form__item');
+
+  selectNotebookContainer.classList.remove('form__item');
+  selectNotebookContainer.classList.add('noDisplay');
+})
+
 const parent = document.getElementById("select_devices");
 
 const devicesList = {
@@ -22,11 +33,15 @@ selectDevice.addEventListener('change', () => {
   const selectNotebookContainer = document.getElementById('selectNotebookModel');
 
   selectPhoneContainer.classList.add('noDisplay');
+  selectPhoneContainer.classList.remove('form__item');
+
+  selectNotebookContainer.classList.remove('form__item');
   selectNotebookContainer.classList.add('noDisplay');
   switch (currentValue) {
       case 'Ноутбуки':
         console.log('выбран ноут')
         selectNotebookContainer.classList.remove('noDisplay');
+        selectNotebookContainer.classList.add('form__item');
         break
       case 'Смарт часы':
         console.log('выбраны часы')
@@ -37,6 +52,7 @@ selectDevice.addEventListener('change', () => {
       case 'Смартфоны':
         console.log('выбраны смартфоны')
         selectPhoneContainer.classList.remove('noDisplay');
+        selectPhoneContainer.classList.add('form__item');
         break
     }
   }
