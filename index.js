b4983e72a65377c50ef6cf0da6d0cae40335c60d
diiimmmaaas <dm.sokolov1994@gmail.com>
2023-03-28 19:00:15 +0300

    add switch notebooks

--- index.js
+++ index.js
@@ -10,93 +10,119 @@ window.addEventListener('load', () => {
       console.log(pair[0]+ ', '+ pair[1]);
     }
   }
-})
 
-const contentBlock = document.getElementById('content');
-const selectDeviceConfigContainer = document.getElementById('selectDeviceConfigContainer');
-const selectDevices = document.getElementById('select_devices');
-const selectDeveloperSmartphones = document.getElementById('selectSmartphonesContainer');
-const selectPhoneModelBlock = document.getElementById('selectPhoneModelBlock');
-const selectPhoneModel = document.getElementById('select_model');
-const selectSmartphones = document.getElementById('select_smartphones');
-const selectWatches = document.getElementById('select_watches');
-const selectNotebook = document.getElementById('select_notebook');
-const nameBlock = document.getElementById('name_block');
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+  const contentBlock = document.getElementById('content');
+  const nameBlock = document.getElementById('name_block');
+  const selectWatchContainer = document.getElementById('selectWatchContainer');
+  const selectSmartphonesContainer = document.getElementById('selectSmartphonesContainer');
+  const selectPhoneModelBlock = document.getElementById('selectPhoneModelBlock');
+  const selectNotebookContainer = document.getElementById('selectNotebookContainer');
+  const selectDeviceConfigContainer = document.getElementById('selectDeviceConfigContainer');
+
+
+
+  const selectDevices = document.getElementById('select_devices');
+  const selectPhoneModel = document.getElementById('select_model');
+  const selectSmartphones = document.getElementById('select_smartphones');
+  const selectWatches = document.getElementById('select_watches');
+  const selectNotebookModel = document.getElementById('select_notebookModel');
+
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
   }
-}
 
-function createNotebookForm(){
-  selectNotebook.classList.remove('noDisplay')
-  nameBlock.classList.remove('noDisplay')
-}
-
-function createSmartphonesForm(){
-  selectDeviceConfigContainer.classList.remove('noDisplay')
-  nameBlock.classList.remove('noDisplay')
-}
-
-function createSmartphoneSelect(object){
-  selectPhoneModelBlock.classList.remove('noDisplay')
-  removeAllChildNodes(selectPhoneModel)
-  for (const key in object) {
-    const option = document.createElement("option");
-    option.classList.add("select_devices__option");
-    option.innerText = `${object[key].value}`;
-    selectPhoneModel.appendChild(option);
+  function createSmartphoneSelect(object){
+    selectPhoneModelBlock.classList.remove('noDisplay')
+    removeAllChildNodes(selectPhoneModel)
+    for (const key in object) {
+      const option = document.createElement("option");
+      option.classList.add("select_devices__option");
+      option.innerText = `${object[key].value}`;
+      selectPhoneModel.appendChild(option);
+    }
+    selectDeviceConfigContainer.classList.remove('noDisplay')
+    nameBlock.classList.remove('noDisplay')
   }
-  createSmartphonesForm()
-}
 
-selectSmartphones.addEventListener('change', () => {
-  let currentValue = selectSmartphones.options[selectSmartphones.selectedIndex].text;
+  selectDevices.addEventListener('change', () => {
+        let currentValue = selectDevices.options[selectDevices.selectedIndex].text;
 
-  switch (currentValue) {
-    case '(не установлено)':
-      selectPhoneModelBlock.classList.add('noDisplay')
-      selectDeviceConfigContainer.classList.add('noDisplay')
-      nameBlock.classList.add('noDisplay')
-      break
-    case 'Apple':
-      createSmartphoneSelect(iphones)
-      break
-    case 'Samsung':
-      createSmartphoneSelect(samsungPhones)
-      break
-    case 'Xiaomi':
+        switch (currentValue) {
+          case '(не установлено)':
+            contentBlock.classList.add('noDisplay')
+            break
+          case 'Ноутбуки':
+            contentBlock.classList.remove('noDisplay')
+            selectWatchContainer.classList.add('noDisplay')
+            selectSmartphonesContainer.classList.add('noDisplay')
+            selectPhoneModelBlock.classList.add('noDisplay')
+            selectNotebookContainer.classList.remove('noDisplay')
+            break
+          case 'Смарт часы':
+            contentBlock.classList.remove('noDisplay')
+            selectWatchContainer.classList.remove('noDisplay')
+            selectSmartphonesContainer.classList.add('noDisplay')
+            selectPhoneModelBlock.classList.add('noDisplay')
+            selectNotebookContainer.classList.add('noDisplay')
+            break
+          case 'Планшеты':
+            contentBlock.classList.remove('noDisplay')
+            break
+          case 'Смартфоны':
+            contentBlock.classList.remove('noDisplay')
+            selectWatchContainer.classList.add('noDisplay')
+            selectSmartphonesContainer.classList.remove('noDisplay')
+            selectPhoneModelBlock.classList.add('noDisplay')
+            selectNotebookContainer.classList.add('noDisplay')
+            break
+        }
+      }
+  )
 
-      break
-    case 'Huawei':
-
-      break
-  }
-})
-
-selectDevices.addEventListener('change', () => {
-    let currentValue = selectDevices.options[selectDevices.selectedIndex].text;
+  selectSmartphones.addEventListener('change', () => {
+    let currentValue = selectSmartphones.options[selectSmartphones.selectedIndex].text;
 
     switch (currentValue) {
       case '(не установлено)':
-        contentBlock.classList.add('noDisplay')
+        selectPhoneModelBlock.classList.add('noDisplay')
+        selectDeviceConfigContainer.classList.add('noDisplay')
+        nameBlock.classList.add('noDisplay')
         break
-      case 'Ноутбуки':
-        contentBlock.classList.remove('noDisplay')
-        selectNotebook.classList.remove('noDisplay')
-        selectDeveloperSmartphones.classList.add('noDisplay')
+      case 'Apple':
+        createSmartphoneSelect(iphones)
         break
-      case 'Смарт часы':
-        contentBlock.classList.remove('noDisplay')
+      case 'Samsung':
+        createSmartphoneSelect(samsungPhones)
         break
-      case 'Планшеты':
-        contentBlock.classList.remove('noDisplay')
+      case 'Xiaomi':
+
         break
-      case 'Смартфоны':
-        contentBlock.classList.remove('noDisplay')
-        selectDeveloperSmartphones.classList.remove('noDisplay')
+      case 'Huawei':
+
+        break
+      case 'OnePlus':
+
         break
     }
-  }
-)
+  })
+
+  selectNotebookModel.addEventListener('change', () => {
+    let currentValue = selectNotebookModel.options[selectNotebookModel.selectedIndex].text;
+
+    switch (currentValue) {
+      case '(не установлено)':
+        break
+      case 'Apple':
+        break
+      case 'Другой производитель':
+        break
+    }
+  })
+})
+
+
+
+
