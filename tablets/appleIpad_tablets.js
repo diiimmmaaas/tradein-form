878d83b50b538b509bc8432943c15126037249a4
vlad <nekita.avdoshin@gmail.com>
2023-03-30 13:16:33 +0300

    add samsung and apple tablets

---
+++ tablets/appleIpad_tablets.js
@@ -0,0 +1,67 @@
+const appleIpad_tablets = {
+  1: {
+    value: "iPad",
+    year: '2022',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  2: {
+    value: "iPad Pro",
+    year: '2022',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  3: {
+    value: "iPad Air",
+    year: '2022',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  4: {
+    value: "iPad mini",
+    year: '2021',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  5: {
+    value: "iPad",
+    year: '2021',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  6: {
+    value: "iPad Pro 12,9",
+    year: '2021',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  7: {
+    value: "iPad Pro 11",
+    year: '2021',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  8: {
+    value: "iPad Air",
+    year: '2020',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+  9: {
+    value: "iPad",
+    year: '2020',
+    versions: {
+      1: {memory: 128, count: ""},
+    },
+  },
+
+
+};
