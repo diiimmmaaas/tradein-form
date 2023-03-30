7b0efc0165f010695920554acb2c4966c075e2db
vlad <nekita.avdoshin@gmail.com>
2023-03-30 13:43:40 +0300

    add samsung and apple watch

---
+++ watch/apple_watch.js
@@ -0,0 +1,189 @@
+// <option value="MacbookPro13">Apple Watch Ultra</option>
+// <option value="MacbookPro13">Apple Watch Series 8</option>
+// <option value="MacbookPro13">Apple Watch SE (2-го поколения)</option>
+// <option value="MacbookPro13">Apple Watch Series 7</option>
+// <option value="MacbookPro13">Apple Watch Nike</option>
+// <option value="MacbookPro13">Apple Watch Series 6</option>
+// <option value="MacbookPro13">Apple Watch SE</option>
+// <option value="MacbookPro13">Apple Watch Series 5</option>
+// <option value="MacbookPro13">Apple Watch Series 4</option>
+// <option value="MacbookPro13">Apple Watch Series 3</option>
+
+const apple_watch = {
+  1: {
+    value: "Apple Watch Ultra",
+    versions: {
+      1: {size: '49mm'},
+    },
+  },
+  2: {
+    value: "Apple Watch Series 8",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  3: {
+    value: "Apple Watch Series 8 Hermès",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  4: {
+    value: "Apple Watch SE (2-го поколения)",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  5: {
+    value: "Apple Watch Series 7",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  6: {
+    value: "Apple Watch Series 7 Nike",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  7: {
+    value: "Apple Watch Series 7 Hermès",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  8: {
+    value: "Apple Watch Series 7 Edition",
+    versions: {
+      1: {size: '41mm'},
+      2: {size: '45mm'},
+    },
+  },
+  9: {
+    value: "Apple Watch Series 6",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  10: {
+    value: "Apple Watch Series 6 Nike",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  11: {
+    value: "Apple Watch Series 6 Hermès",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  12: {
+    value: "Apple Watch Series 6 Edition",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  13: {
+    value: "Apple Watch SE",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  14: {
+    value: "Apple Watch SE Nike",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  15: {
+    value: "Apple Watch Series 5",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  16: {
+    value: "Apple Watch Series 5 Nike",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  17: {
+    value: "Apple Watch Series 5 Hermès",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  18: {
+    value: "Apple Watch Series 5 Edition",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  19: {
+    value: "Apple Watch Series 4",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  20: {
+    value: "Apple Watch Series 4 Nike+",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  21: {
+    value: "Apple Watch Series 4 Hermès",
+    versions: {
+      1: {size: '40mm'},
+      2: {size: '44mm'},
+    },
+  },
+  22: {
+    value: "Apple Watch Series 3",
+    versions: {
+      1: {size: '38mm'},
+      2: {size: '42mm'},
+    },
+  },
+  23: {
+    value: "Apple Watch Series 3 Nike+",
+    versions: {
+      1: {size: '38mm'},
+      2: {size: '42mm'},
+    },
+  },
+  24: {
+    value: "Apple Watch Series 3 Edition",
+    versions: {
+      1: {size: '38mm'},
+      2: {size: '42mm'},
+    },
+  },
+  24: {
+    value: "Apple Watch Series 3 Hermès",
+    versions: {
+      1: {size: '38mm'},
+      2: {size: '42mm'},
+    },
+  },
+
+
+};
---
+++ watch/samsung_watch.js
@@ -0,0 +1,9 @@
+// <option value="MacbookPro13">Samsung Galaxy Watch5</option>
+// <option value="MacbookPro13">Samsung Galaxy Watch4</option>
+// <option value="MacbookPro13">Samsung Galaxy Watch3</option>
+// <option value="MacbookPro13">Samsung Galaxy Watch Active 2</option>
+// <option value="MacbookPro13">Samsung Galaxy Watch Active</option>
+// <option value="MacbookPro13">Samsung Galaxy Watch</option>
+// <option value="MacbookPro13">Samsung Galaxy Fit</option>
+
+const samsung_watch = {};
