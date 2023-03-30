344fa8ab5919eb82cb3f8e5ae11a8f221e6604c4
vlad <nekita.avdoshin@gmail.com>
2023-03-30 13:47:25 +0300

    add samsung watches

--- watch/samsung_watch.js
+++ watch/samsung_watch.js
@@ -6,4 +6,26 @@
 // <option value="MacbookPro13">Samsung Galaxy Watch</option>
 // <option value="MacbookPro13">Samsung Galaxy Fit</option>
 
-const samsung_watch = {};
+const samsung_watch = {
+  1: {
+    value: "Samsung Galaxy Watch5",
+  },
+  2: {
+    value: "Samsung Galaxy Watch4",
+  },
+  3: {
+    value: "Samsung Galaxy Watch3",
+  },
+  4: {
+    value: "Samsung Galaxy Watch Active 2",
+  },
+  5: {
+    value: "Samsung Galaxy Watch Active",
+  },
+  6: {
+    value: "Samsung Galaxy Watch",
+  },
+  7: {
+    value: "Samsung Galaxy Fit",
+  },
+};
