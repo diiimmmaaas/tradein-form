be0d501783bf3c14aeedfe2167ff37ae2fbf43cd
diiimmmaaas <dm.sokolov1994@gmail.com>
2023-04-04 10:14:28 +0300

    try to fix send mailer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 window.addEventListener("load", () => {
 
   (function() {
@@ -68,19 +70,19 @@ window.addEventListener("load", () => {
 
       // wrapper.classList.add('_sending')
 
-      let response = await fetch('sender.php', {
-        method: 'POST',
-        body: formData
-      })
-      if (response.ok){
-        let result = await response.json()
-        alert(result.message)
-        form.reset()
-        // wrapper.classList.remove('_sending')
-      } else {
-        alert("Ошибка")
-        // wrapper.classList.remove('_sending')
-      }
+      // let response = await fetch('mail.php', {
+      //   method: 'POST',
+      //   body: formData
+      // })
+      // if (response.ok){
+      //   let result = await response.json()
+      //   alert(result.message)
+      //   form.reset()
+      //   // wrapper.classList.remove('_sending')
+      // } else {
+      //   alert("Ошибка")
+      //   // wrapper.classList.remove('_sending')
+      // }
     } else {
       alert("Заполните обязательные поля");
     }
